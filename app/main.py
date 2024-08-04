6492e9f293c92c510e7fb85ff2e829f5dd06b31a
beshketnyck <eelon.max@gmail.com>
2024-08-04 22:16:43 +0300

    Add functionality to add expenses with category selection

--- app/main.py
+++ app/main.py
@@ -1,3 +1,4 @@
+# Імпортуйте необхідні модулі
 from flask import Flask, render_template, request, redirect, url_for
 from models.expense import Expense
 from models.category import Category
@@ -7,20 +8,9 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
-    expenses = Expense.query.all()
     categories = Category.query.all()
-    return render_template('index.html', expenses=expenses, categories=categories)
-
-@app.route('/add_expense', methods=['POST'])
-def add_expense():
-    amount = request.form['amount']
-    category_id = request.form['category_id']
-    comment = request.form['comment']
-    date = request.form['date']
-    expense = Expense(amount=amount, category_id=category_id, comment=comment, date=date)
-    db_session.add(expense)
-    db_session.commit()
-    return redirect(url_for('index'))
+    expenses = Expense.query.all()
+    return render_template('index.html', categories=categories, expenses=expenses)
 
 @app.route('/add_category', methods=['POST'])
 def add_category():
@@ -30,6 +20,26 @@ def add_category():
     db_session.commit()
     return redirect(url_for('index'))
 
+@app.route('/add_expense', methods=['POST'])
+def add_expense():
+    amount = request.form['amount']
+    category_id = request.form['category_id']
+    comment = request.form['comment']
+    date = request.form['date']
+    if not category_id:
+        category_id = 1  # ID категорії "без категорії", припускаємо, що це перший запис у таблиці
+    expense = Expense(amount=amount, category_id=category_id, comment=comment, date=date)
+    db_session.add(expense)
+    db_session.commit()
+    return redirect(url_for('index'))
+
 if __name__ == '__main__':
     init_db()
+
+    # Створення категорії "без категорії", якщо ще не існує
+    if not Category.query.filter_by(name='Без категорії').first():
+        default_category = Category(name='Без категорії')
+        db_session.add(default_category)
+        db_session.commit()
+
     app.run(debug=True)
